8d5301293a03df11937e2c199054d519ce98dfb2
Al Amin Sarker Dolon <80084331+alamindolon@users.noreply.github.com>
2023-03-06 08:42:36 +0600

    html part done

---
+++ js/app.js
@@ -0,0 +1,13 @@
+var carobject = {
+    vehicle: 'bus',
+    imageurl: 'https://images.unsplash.com/photo-1503376780353-7e6692767b70?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8Y2FyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    farePerkilo : 3,
+    description: 'lorem this bus is very comfortable in journey for user'
+  }
+
+
+
+  function displayService(service)
+  {
+    const mainSection = document.getElementById('main_section');
+  }
